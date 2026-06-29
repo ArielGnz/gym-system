dd87697af05e8d8de2775ae86a2072425b65831d
ArielGnz <arielgon2206@gmail.com>
2026-06-29 20:08:04 -0300

    database connect

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,21 @@
 const app = require("./app");
+const sequelize = require("./config/database");
 
 const PORT = 3001;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+async function startServer() {
+  try {
+    await sequelize.authenticate();
+
+    console.log("Base de datos conectada");
+
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en puerto ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error al conectar la base de datos");
+    console.error(error.message);
+  }
+}
+
+startServer();
