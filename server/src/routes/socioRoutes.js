2130ea36aabd91d73d8ddcfe0a2ce6fe8d64290e
ArielGnz <arielgon2206@gmail.com>
2026-07-10 20:26:09 -0300

    ruta de socio

--- server/src/routes/socioRoutes.js
+++ server/src/routes/socioRoutes.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
-const { crearSocio } = require("../controllers/socioControllers");
+const { crearSocio, obtenerSocios } = require("../controllers/socioControllers");
 
 const router = Router();
 
 router.post("/", crearSocio);
-
+router.get("/", obtenerSocios);
 
 module.exports = router;
