9abd537e4abe9ffb1fbe48a3734684d1723a9f09
ArielGnz <arielgon2206@gmail.com>
2026-07-13 19:17:36 -0300

    form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,11 @@ import api from './services/api';
 function App() {
   const [socios, setSocios] = useState([]);
 
+  const [nombre, setNombre] = useState("");
+const [apellido, setApellido] = useState("");
+const [dni, setDni] = useState("");
+const [telefono, setTelefono] = useState("");
+
   useEffect(() =>{
     obtenerSocios();
   }, []);
@@ -16,12 +21,22 @@ function App() {
       console.error("Error al obtener los socios:", error);
     }
   };
-  
+
 
   return (
-    <>
+    <div>
       <h1>Sistema de Gimnasio</h1>
-    </>
+
+      {
+        socios.map((socio) => (
+          <div key={socio.id}>
+            <h2>{socio.nombre} {socio.apellido}</h2>
+            <p>Edad: {socio.edad}</p>
+          </div>
+        ))
+      }
+
+    </div>
   );
 }
 
