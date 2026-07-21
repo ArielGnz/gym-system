55bf162adbd82e3d4eacadd91a4d0d4167bf1728
ArielGnz <arielgon2206@gmail.com>
2026-07-21 10:44:51 -0300

    inputs edit

--- client/src/components/ModalEditarSocio.jsx
+++ client/src/components/ModalEditarSocio.jsx
@@ -11,7 +11,44 @@ function ModalEditarSocio ({ socio, setSocioEditando, obtenersocios }) {
     return (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
             <div className="bg-white p-6 rounded-xl w-full max-w-md">
+                <div className="flex justify-end">
+                    <button onClick={() => setSocioEditando(null)}
+                        className="text-gray-500 text-xl"> ✕
+                    </button>
+                </div>
                 <h2 className="text-2xl font-bold mb-4">Editar Socio</h2>
+                <div className="space-y-3">
+                    <input 
+                        type="text"
+                        value={nombre}
+                        onchange={(e) => setNombre(e.target.value)}
+                        placeholder="Nombre"
+                        className="w-full border rounded-lg px-3 py-2"
+                    />
+
+                    <input 
+                        type="text"
+                        value={apellido}
+                        onchange={(e) => setApellido(e.target.value)}
+                        placeholder="Apellido"
+                        className="w-full border rounded-lg px-3 py-2"
+                    />
+                    <input 
+                        type="text"
+                        value={dni}
+                        onchange={(e) => setDni(e.target.value)}
+                        placeholder="DNI"
+                        className="w-full border rounded-lg px-3 py-2"
+                    />
+                    <input 
+                        type="text"
+                        value={telefono}
+                        onchange={(e) => setTelefono(e.target.value)}
+                        placeholder="Teléfono"
+                        className="w-full border rounded-lg px-3 py-2"
+                    />
+
+                </div>
             </div>
         </div>
     )
