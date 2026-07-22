4b524a1525bc0b383d3b9034c2ad6faa2a1539d2
ArielGnz <arielgon2206@gmail.com>
2026-07-22 20:05:56 -0300

    Header creado

---
+++ client/src/components/layout/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="bg-white shadow px-6 py-4">
+      <h1 className="text-2xl font-bold">
+        Sistema de Gimnasio
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
