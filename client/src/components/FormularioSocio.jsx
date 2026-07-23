58b6755a448f90c4e8c2be84e2cf50ba3d4a98da
ArielGnz <arielgon2206@gmail.com>
2026-07-23 12:26:10 -0300

    importando layaout

--- client/src/components/FormularioSocio.jsx
+++ client/src/components/FormularioSocio.jsx
@@ -58,7 +58,7 @@ function FormularioSocio({
       <br />
 
       <button onClick={guardarSocio}
-      className="bg-blue-600 text-white px-4 py-2 rounded-lg cursor-pointer">
+        className="bg-blue-600 text-white px-4 py-2 rounded-lg cursor-pointer">
         Guardar Socio
       </button>
     </div>
--- client/src/pages/Socios.jsx
+++ client/src/pages/Socios.jsx
@@ -4,6 +4,7 @@ import { useSocios } from "../hooks/useSocios";
 import FormularioSocio from "../components/FormularioSocio";
 import ListaSocios from "../components/ListaSocios";
 import ModalEditarSocio from '../components/ModalEditarSocio';
+import Layout from '../components/layout/Layout';
 
 function Socios() {
 
@@ -54,54 +55,56 @@ function Socios() {
 
 
   return (
-    <div className="min-h-screen bg-slate-100">
-      <div className="max-w-6xl mx-auto p-6">
-        <h1 className="text-4xl font-bold mb-8">
-          Sistema de Gimnasio
-        </h1>
+    <Layout>
+        <div className="min-h-screen bg-slate-100">
+            <div className="max-w-6xl mx-auto p-6">
+                <h1 className="text-4xl font-bold mb-8">
+                Sistema de Gimnasio
+                </h1>
 
-        <FormularioSocio
-          nombre={nombre}
-          setNombre={setNombre}
-          apellido={apellido}
-          setApellido={setApellido}
-          dni={dni}
-          setDni={setDni}
-          telefono={telefono}
-          setTelefono={setTelefono}
-          guardarSocio={handleGuardarSocio}
-        />
+                <FormularioSocio
+                nombre={nombre}
+                setNombre={setNombre}
+                apellido={apellido}
+                setApellido={setApellido}
+                dni={dni}
+                setDni={setDni}
+                telefono={telefono}
+                setTelefono={setTelefono}
+                guardarSocio={handleGuardarSocio}
+                />
 
-        <div className="mb-6">
-          <input 
-            type="text"
-            placeholder="Buscar socio..."
-            value={busqueda}
-            onChange={(e) => setBusqueda(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded"
-          />
+                <div className="mb-6">
+                <input 
+                    type="text"
+                    placeholder="Buscar socio..."
+                    value={busqueda}
+                    onChange={(e) => setBusqueda(e.target.value)}
+                    className="w-full p-2 border border-gray-300 rounded"
+                />
+                </div>
+
+                <div>
+                <h1>Socios encontrados: {sociosFiltrados.length}</h1>
+                </div>
+
+                <ListaSocios 
+                socios={sociosFiltrados} 
+                eliminarSocio={eliminarSocio}
+                setSocioEditando={setSocioEditando}   
+                />
+
+                {socioEditando && (
+                <ModalEditarSocio
+                    socio = {socioEditando}
+                    setSocioEditando = {setSocioEditando}
+                    obtenerSocios = {obtenerSocios}
+                />
+                )}
+
+            </div>
         </div>
-
-        <div>
-          <h1>Socios encontrados: {sociosFiltrados.length}</h1>
-        </div>
-
-        <ListaSocios 
-          socios={sociosFiltrados} 
-          eliminarSocio={eliminarSocio}
-          setSocioEditando={setSocioEditando}   
-        />
-
-        {socioEditando && (
-          <ModalEditarSocio
-            socio = {socioEditando}
-            setSocioEditando = {setSocioEditando}
-            obtenerSocios = {obtenerSocios}
-          />
-        )}
-
-      </div>
-    </div>
+    </Layout>
 );
 }
 
