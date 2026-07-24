8b2de1b63bda25bc4f3530e0e51a51bc911f9f47
ArielGnz <arielgon2206@gmail.com>
2026-07-24 18:24:14 -0300

    rutas links de dashboard listo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 
 import { Routes, Route } from 'react-router-dom';
 import Socios from './pages/Socios';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   
   return (
     <Routes>
-      <Route path="/" element={<Socios />} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="/socios" element={<Socios />} />
 
     </Routes>
-);
+  );
 }
 
 export default App
