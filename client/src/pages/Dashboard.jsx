376f1f411b268926b248eaebe7bea608742cf8af
ArielGnz <arielgon2206@gmail.com>
2026-07-24 18:22:50 -0300

    dashboard inicio

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import Layout from "../components/layout/Layout";
+
+function Dashboard() {
+    return (
+        <Layout>
+            <h1 className="text-3xl font-bold mb-6">
+                Dashboard
+            </h1>
+        </Layout>
+    )
+}
+
+export default Dashboard;
