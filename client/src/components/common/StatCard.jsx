5b8899b49265411f940f60b19b02aa2420417945
ArielGnz <arielgon2206@gmail.com>
2026-07-24 19:42:05 -0300

    statCard

---
+++ client/src/components/common/StatCard.jsx
@@ -0,0 +1,23 @@
+const StatCard = ({ titulo, valor, icono}) =>{
+    return (
+        <div className="bg-white rounded-xl shadow-md p-6">
+            <div className="flex justify-between items-center">
+                <div>
+                <p className="text-gray-500">
+                    {titulo}
+                </p>
+
+                <h2 className="text-3xl font-bold mt-2">
+                    {valor}
+                </h2>
+                </div>
+
+                <div className="text-4xl">
+                {icono}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StatCard;
