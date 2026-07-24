24bf9e5df3502fbebd07693a883b2267b9a912c3
ArielGnz <arielgon2206@gmail.com>
2026-07-24 19:46:47 -0300

    StatCards en Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,6 @@
 import Layout from "../components/layout/Layout";
+import StatCard from "../components/common/StatCard";
+import { FaUsers, FaMoneyBill, FaDumbbell } from "react-icons/fa";
 
 function Dashboard() {
     return (
@@ -6,6 +8,21 @@ function Dashboard() {
             <h1 className="text-3xl font-bold mb-6">
                 Dashboard
             </h1>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <StatCard
+                    titulo="Total Socios"
+                    valor=""
+                    icono={<FaUsers />} />
+                <StatCard
+                    titulo="Pagos"
+                    valor="0"
+                    icono={<FaMoneyBill />} />
+                <StatCard
+                    titulo="Rutinas"
+                    valor="0"
+                    icono={<FaDumbbell />} />
+            </div>
         </Layout>
     )
 }
