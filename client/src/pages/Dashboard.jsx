7834a20aa56b9b914f328dd6b780e097ca096c69
ArielGnz <arielgon2206@gmail.com>
2026-07-24 20:06:22 -0300

    Stat en dashboard listo

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,25 @@ import StatCard from "../components/common/StatCard";
 import { FaUsers, FaMoneyBill, FaDumbbell } from "react-icons/fa";
 
 function Dashboard() {
+
+    const estadisticas = [
+        {
+            titulo: "Total Socios",
+            valor: "0",
+            icono: <FaUsers />
+        },
+        {
+            titulo: "Pagos",
+            valor: "0",
+            icono: <FaMoneyBill />
+        },
+        {
+            titulo: "Rutinas",
+            valor: "0",
+            icono: <FaDumbbell />
+        }
+    ];
+
     return (
         <Layout>
             <h1 className="text-3xl font-bold mb-6">
@@ -10,18 +29,14 @@ function Dashboard() {
             </h1>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <StatCard
-                    titulo="Total Socios"
-                    valor=""
-                    icono={<FaUsers />} />
-                <StatCard
-                    titulo="Pagos"
-                    valor="0"
-                    icono={<FaMoneyBill />} />
-                <StatCard
-                    titulo="Rutinas"
-                    valor="0"
-                    icono={<FaDumbbell />} />
+                {estadisticas.map((item) => (
+                    <StatCard
+                        key={item.titulo}
+                        titulo={item.titulo}
+                        valor={item.valor}
+                        icono={item.icono}
+                    />
+                ))}
             </div>
         </Layout>
     )
