5f82e84f20ad36412b3b0b5c9114a89234ffe5b5
ArielGnz <arielgon2206@gmail.com>
2026-07-27 20:15:14 -0300

    dates

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,9 +1,18 @@
+import { formatearFecha } from "../../utils/formatearFecha";
+
 function Header() {
+
+  const fechaActual = formatearFecha(new Date());
+
   return (
-    <header className="bg-white shadow px-6 py-4">
+    <header className="bg-white shadow px-6 py-4 flex justify-between items-center">
       <h1 className="text-2xl font-bold">
         Sistema de Gimnasio
       </h1>
+
+      <span className="text-gray-500">
+        {fechaActual}
+      </span>
     </header>
   );
 }
