49d698d63e3094c7957a38dab327def5629c7f87
ArielGnz <arielgon2206@gmail.com>
2026-07-27 20:12:50 -0300

    utils fecha

--- client/src/components/layout/Sidebar.jsx
+++ client/src/components/layout/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import  logo  from "../../assets/logo.png";
-import menu from "../../constants/menu";
+import { menu } from "../../constants/menu";
 
 function Sidebar() {
 
---
+++ client/src/utils/formatearFecha.js
@@ -0,0 +1,8 @@
+export function formatearFecha(fecha) {
+  return new Intl.DateTimeFormat("es-AR", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(fecha);
+}
