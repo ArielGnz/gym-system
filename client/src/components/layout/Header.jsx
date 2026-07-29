9984e28a4d5a829c9b86610a4a3991a590bd8069
ArielGnz <arielgon2206@gmail.com>
2026-07-29 09:48:20 -0300

    Header actual success

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,20 +1,50 @@
-import { formatearFecha } from "../../utils/formatearFecha";
+
+import { FaBell, FaUserCircle } from "react-icons/fa";
 
 function Header() {
+    return (
+        <header className="bg-white shadow-sm px-8 py-4 flex justify-between items-center">
 
-  const fechaActual = formatearFecha(new Date());
+            <div>
+                <h1 className="text-2xl font-bold">
+                  Sistema de Gestion
+                </h1>
+            </div>
 
-  return (
-    <header className="bg-white shadow px-6 py-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold">
-        Sistema de Gimnasio
-      </h1>
+            <div className="flex items-center gap-6">
 
-      <span className="text-gray-500">
-        {fechaActual}
-      </span>
-    </header>
-  );
+                <button className="relative text-2xl text-slate-600 hover:text-blue-600 transition-colors cursor-pointer">
+
+                    <FaBell />
+
+                    <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
+                        3
+                    </span>
+
+                </button>
+
+                <div className="flex items-center gap-2">
+
+                    <FaUserCircle className="text-3xl text-slate-600" />
+
+                    <div>
+
+                        <p className="font-semibold">
+                            Ariel
+                        </p>
+
+                        <p className="text-sm text-gray-500">
+                            Administrador
+                        </p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </header>
+    );
 }
 
 export default Header;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import Layout from "../components/layout/Layout";
 import StatCard from "../components/common/StatCard";
+import WelcomeCard from "./Dashboard/WelcomeCard";
 import { FaUsers, FaMoneyBill, FaDumbbell } from "react-icons/fa";
 import { useSocios } from "../hooks/useSocios";
 import { useEffect } from "react";
@@ -38,10 +39,9 @@ function Dashboard() {
 
     return (
         <Layout>
-            <h1 className="text-3xl font-bold mb-6">
-                Dashboard
-            </h1>
 
+            <WelcomeCard/>
+            
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 {estadisticas.map((item) => (
                     <StatCard
