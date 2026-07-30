df6e50e55cf44b6b55446d114c9b50c873ba0f6d
ArielGnz <arielgon2206@gmail.com>
2026-07-30 18:57:01 -0300

    sucursal controller lista CRUD

--- server/src/controllers/sucursalControllers.js
+++ server/src/controllers/sucursalControllers.js
@@ -8,3 +8,40 @@ const crearSucursal = async (req, res) => {
         res.status(500).json({error: error.message});
     }
 };
+
+const obtenerSucursales = async (req, res) => {
+    try {
+        const sucursales = await Sucursal.findAll({order: [["nombre", "ASC"]], });
+        res.json(sucursales);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const eliminarSucursal = async (req,res) => {
+    try {
+        const { id } = req.params;
+        await Sucursal.destroy({where: { id }, });
+        res.json({ message: "Sucursal Eliminada"});
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+const actualizarSucursal = (req, res) => {
+    try {
+        const { id } = req.params;
+        await Sucursal.update(req.body, { where: {id},});
+        res.json({ message: "Sucursal Actualizada"});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = {
+    crearSucursal,
+    actualizarSucursal,
+    eliminarSucursal,
+    obtenerSucursales,
+}
