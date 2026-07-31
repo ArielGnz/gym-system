27a3e386751d1fba8bf9ed7a77cacff580ef8d1e
ArielGnz <arielgon2206@gmail.com>
2026-07-31 09:48:49 -0300

    hook de sucursales completo

--- client/src/hooks/useSucursales.js
+++ client/src/hooks/useSucursales.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import api from "../services/api";
+import { eliminarSocio } from "../../../server/src/controllers/socioControllers";
+import { eliminarSucursal } from "../../../server/src/controllers/sucursalControllers";
 
 export function useSucursales() {
     
@@ -22,5 +24,31 @@ export function useSucursales() {
             console.error(error);
         }
     };
-    
+
+    const elminiarSucursal = async (id) => {
+        try {
+            await api.delete(`/sucursales/${id}`);
+            await obtenerSucursales();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const actualzarSucursales = async (id, datos) => {
+        try {
+            await api.put(`/sucursales/${id}`, datos);
+            await obtenerSucursales();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return {
+        sucursales,
+        obtenerSucursales,
+        guardarSucursales,
+        eliminarSucursal,
+        actualzarSucursales,
+    };
+
 }
