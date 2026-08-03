e6eeb654bbc299bac32a46bc29d5163487b11222
ArielGnz <arielgon2206@gmail.com>
2026-08-03 20:11:10 -0300

    models plan

---
+++ server/src/models/Plan.js
@@ -0,0 +1,34 @@
+const {  DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const Plan = sequelize.define(
+    "Plan",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false,   
+        },
+        descripcion: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        precio: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+        },
+        duracionDias: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        tableName: "planes",
+    }
+);
+
+module.exports = Plan;
