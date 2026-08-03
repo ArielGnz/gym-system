5531ced0cf4e2a81750cd4777421ca1d51c0a897
ArielGnz <arielgon2206@gmail.com>
2026-08-03 20:26:12 -0300

    index model listo

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,3 +1,33 @@
 const Socio = require("./Socio");
 const Sucursal = require("./SucursalModel");
 const Plan = require("./Plan"); 
+
+Sucursal.hasMany(Socio, {
+  foreignKey: "sucursalId",
+});
+
+Socio.belongsTo(Sucursal, {
+  foreignKey: "sucursalId",
+});
+
+Sucursal.hasMany(Plan, {
+    foreignKey: "sucursalId",
+});
+
+Plan.belongsTo(Sucursal, {
+    foreignKey: "sucursalId",
+});
+
+Plan.hasMany(Socio, {
+    foreignKey: "planId",
+});
+
+Socio.belongsTo(Plan, {
+    foreignKey: "planId",
+});
+
+module.exports = {
+  Socio,
+  Sucursal,
+  Plan,
+};
