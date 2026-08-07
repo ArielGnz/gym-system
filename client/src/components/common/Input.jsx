82c4188660f0aa4a8c121af57c0d031d0fac35a4
ArielGnz <arielgon2206@gmail.com>
2026-08-07 19:30:07 -0300

    components input

---
+++ client/src/components/common/Input.jsx
@@ -0,0 +1,26 @@
+
+
+const Input = ({
+    label,
+    value,
+    onChange,
+    placeholder,
+    type = "text",
+}) => {
+    return (
+        <div className="mb-4">
+            {label && (
+                <label className="block mb-1 font-medium">{label}</label>
+            )}
+
+            <input 
+                type={type}
+                value={value}
+                onChange={onChange}
+                placeholder={placeholder}
+                className="w-full border rounded-lg px-3 py-2 "
+            />
+        </div>
+    );
+}
+        
