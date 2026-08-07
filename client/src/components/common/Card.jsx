ec3c0816edb835830fabe571754e3b55fad60d18
ArielGnz <arielgon2206@gmail.com>
2026-08-07 19:52:26 -0300

    Card components

---
+++ client/src/components/common/Card.jsx
@@ -0,0 +1,10 @@
+const Card = ({children}) => {
+    return (
+        <div clasName="bg-white rounded-lg shadow-md p-6">
+            {children}
+        </div>
+    );
+
+}
+
+export default Card;
