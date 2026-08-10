fd68cbf77667fc38239898ed87636c28a1159c78
ArielGnz <arielgon2206@gmail.com>
2026-08-10 11:47:04 -0300

    hook de use planes

---
+++ client/src/hooks/usePlanes.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import api from "../services/api";
+
+const usePlanes = () => {
+    const [planes, setPlanes] = useState([]);
+
+    const obtenerPlanes = async () => {
+        try {
+            const response = await api.get("/planes");
+            setPlanes(response.data);
+        } catch (error) {
+            console.error("Error al obtener los planes:", error);
+        }
+    }
+
+    const guardarPlan = async (plan) => {
+        try {
+            await api.post("/planes", plan);
+            await obtenerPlanes();
+        } catch (error) {
+            console.error("Error al guardar el plan:", error);
+        }
+    }
+
+    const eliminarPlan = async (id) => {
+        try {
+            await api.delete(`/planes/${id}`);
+            await obtenerPlanes();
+        } catch (error) {
+            console.error("Error al eliminar el plan:", error);
+        }
+    }
+
+    
+
+    
+}
