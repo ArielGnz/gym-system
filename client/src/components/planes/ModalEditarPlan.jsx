898118b369fb134fba5facf1ffbe21bb2ed500dd
ArielGnz <arielgon2206@gmail.com>
2026-08-11 12:05:27 -0300

    modal editar planes en procesao

---
+++ client/src/components/planes/ModalEditarPlan.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import Input from "../common/Input";
+import Select from "../common/Select";
+import Button from "../common/Button";
+import Modal from "../common/Modal";
+
+const ModalEditarPlan = ({
+    plan, 
+    setPlanEditando, 
+    actualizarPlan, 
+    sucursal}) => {
+
+    const [nombre, setNombre] = useState(plan.nombre);
+    const [descripcion, setDescripcion] = useState(plan.descripcion);
+    const [precio, setPrecio] = useState(plan.precio);
+    const [duracionDias, setDuracionDias] = useState(plan.duracionDias);
+    const [sucursalId, setSucursalId] = useState(plan.sucursalId);   
+
+}
