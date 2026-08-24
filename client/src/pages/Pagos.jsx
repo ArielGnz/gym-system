9c70b5c7099edba7494118124f23063409641aff
ArielGnz <arielgon2206@gmail.com>
2026-08-24 19:49:28 -0300

    fechas cargadas en pagos.jsx

--- client/src/pages/Pagos.jsx
+++ client/src/pages/Pagos.jsx
@@ -111,7 +111,33 @@ const Pagos = () => {
 
                 {/* Fecha de Pago */}
 
-                div
+                <div className="mb-4">
+                    <label className="block mb-1 font-medium">Fecha de Pago</label>
+
+                    <input 
+                        type="date"
+                        value={fechaPago}
+                        onChange={(e) => setFechaPago(e.target.value)}
+                        className="w-full border rounded-lg px-3 py-2"
+                    />
+
+                </div>
+
+                {/* Fecha de Vencimiento */}
+
+                <div className="mb-4">
+                    <label className="block mb-1 font-medium">Fecha de Vencimiento</label>
+
+                    <input 
+                        type="date"
+                        value={fechaVencimiento}
+                        onChange={(e) => setFechaVencimiento(e.target.value)}
+                        className="w-full border rounded-lg px-3 py-2" 
+                    />
+
+                    {/* Importe */}
+
+                </div>
 
             </div>
 
