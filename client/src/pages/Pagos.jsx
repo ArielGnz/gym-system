dc392b2183b51756fbd35e25df06b244567b2147
ArielGnz <arielgon2206@gmail.com>
2026-08-24 19:52:42 -0300

    importe cargado

--- client/src/pages/Pagos.jsx
+++ client/src/pages/Pagos.jsx
@@ -137,6 +137,19 @@ const Pagos = () => {
 
                     {/* Importe */}
 
+                    <div className="mb-4">
+                        <label className="block mb-1 font-medium">Importe</label>
+
+                        <input
+                            type="number"
+                            value={importe}
+                            onChange={(e) => setImporte(e.target.value)}
+                            placeholder="Importe del Pago"
+                            className="w-full border rounded-lg px-3 py-2"
+                        />
+
+                    </div>
+
                 </div>
 
             </div>
