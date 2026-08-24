205d790b84e4c388601c057485ce19ec5ae94a2d
ArielGnz <arielgon2206@gmail.com>
2026-08-24 20:06:29 -0300

    revisando error de codigo en pagosjsx

--- client/src/pages/Pagos.jsx
+++ client/src/pages/Pagos.jsx
@@ -150,6 +150,23 @@ const Pagos = () => {
 
                     </div>
 
+                    {/* Estado */}
+
+                    <div className="mb-4">
+                        <label className="block mb-1 font-medium"> Estado </label>
+
+                        <select 
+                            value={estado}
+                            onChange={(e) => setEstado(e.target.value)}
+                            className="w-full border rounded-lg px-3 py-2"
+                        >
+                            <option value="PAGADO"> PAGADO </option>
+                            <option value="VENCIDO"> VENCIDO </option>
+
+                        </select>
+
+                    </div>
+
                 </div>
 
             </div>
