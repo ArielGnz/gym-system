67f6e5e474ff61ba360b1ff1bf84bd1f353d1e0c
ArielGnz <arielgon2206@gmail.com>
2026-08-27 19:44:50 -0300

    socios en formulario pago

--- client/src/components/pagos/FormularioPago.jsx
+++ client/src/components/pagos/FormularioPago.jsx
@@ -1,3 +1,29 @@
 import Input from "../common/Input";
 import Select from "../common/Select";
 import Button from "../common/Button";
+
+const FormularioPago = ({
+    fechaPago,
+    setFechaPago,
+    fechaVencimiento,
+    setFechaVencimiento,
+    importe,
+    setImporte,
+    estado,
+    setEstado,
+    socioId,
+    setSocioId,
+    planId,
+    setPlanId,
+    socios,
+    planes,
+    guardarPago
+}) => {
+    
+    const opcionesSocios = socios.map((socio) => ({
+        id: socio.id,
+        nombre: `${socio.nombre} ${socio.apellido}`,
+    }) );
+
+    
+}
