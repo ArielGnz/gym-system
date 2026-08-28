3c8db1736e1df32ffdcc505118bb45639972d014
ArielGnz <arielgon2206@gmail.com>
2026-08-28 18:45:12 -0300

    formulario de pago listo

--- client/src/components/pagos/FormularioPago.jsx
+++ client/src/components/pagos/FormularioPago.jsx
@@ -36,9 +36,7 @@ const FormularioPago = ({
                 onChange={(e) => setSocioId(e.target.value)}
                 options={opcionesSocios}
             />
-
-            
-                
+               
             <Select
                 label="plan"
                 value={planId}
@@ -46,14 +44,43 @@ const FormularioPago = ({
                 options={planes}
             />
 
-            <Select 
+            <Input 
                 label="Fecha de Pago"
                 type="date"
                 value={fechaPago}
                 onChange={(e) => setFechaPago(e.target.value)}
             />
-                
+
+            <Input 
+                label="Fecha de Vencimiento"
+                type="date"
+                value={(e) => setFechaVencimiento(e.target.value)}
+            />
+
+            <Input 
+                label="Importe"
+                type="number"
+                placeholder="Importe de pago"
+                value={importe}
+                onChange={(e) => setImporte(e.target.value)}
+            />
+
+            <Select 
+                label="Estado"
+                value={estado}
+                onChange={(e) => setEstado(e.target.value)}
+                options={[
+                    {id: "PAGADO", nombre: "PAGADO"}
+                    {id: "VENCIDO", nombre: "VENCIDO"}
+                ]}
+            />
+
+            <Button onClick={guardarPago}>
+                Registrar Pago
+            </Button>        
 
         </div>
-    )
+    );
 }
+
+export default FormularioPago;
