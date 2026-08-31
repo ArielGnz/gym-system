9c876fe84aaa9d3c965e0ea038f96ec38e38ad5f
ArielGnz <arielgon2206@gmail.com>
2026-08-31 19:59:07 -0300

    Lista de Pagos

---
+++ client/src/components/pagos/ListaPagos.jsx
@@ -0,0 +1,29 @@
+import PagoCard from "./PagoCard";
+
+const ListaPagos = (
+    pagos, 
+    eliminarPago, 
+    setPagoEditando
+) => {
+    return (
+        <div>
+
+            <h2 className="text-xl font-bold mb-4">
+                Lista de Pagos
+            </h2>
+
+            {pagos.map ((pago) => (
+                <PagoCard
+                    key={pago.id}
+                    pago={pago}
+                    eliminarPago={eliminarPago}
+                    setPagoEditando={setPagoEditando}
+                />
+            ))
+
+            }
+        </div>
+    )
+}
+
+export default ListaPagos;
