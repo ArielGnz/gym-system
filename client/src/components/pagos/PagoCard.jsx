a9af7855447b9cb5abb84d4199f7ce27dd9bc9f2
ArielGnz <arielgon2206@gmail.com>
2026-08-31 10:20:44 -0300

    Pago Card listo

--- client/src/components/pagos/PagoCard.jsx
+++ client/src/components/pagos/PagoCard.jsx
@@ -6,6 +6,36 @@ const PagoCard = ({pago, eliminarPago, setPagoEditando}) => {
             <h3 className="font-bold text-lg">
                 {pago.Socio?.nombre} {pago.Socio?.apellido}
             </h3>
+
+            <p>Plan: {pago.Plan?.nombre} </p>
+
+            <p>Fecha de Pago: {pago.fechaPago} </p>
+
+            <p>Vencimiento: {pago.fechaVencimiento} </p>
+
+            <p>Importe: ${pago.importe} </p>
+
+            <p>Estado: {pago.estado} </p>
+
+            <div className="mt-3 flex gap-2">
+                
+                <button
+                    onClick={() => setPagoEditando(pago)}
+                    className="bg-yellow-500 text-white px-4 py-2 rounded-lg"
+                >
+                    Editar
+                </button>
+
+                <button
+                    onClick={() => eliminarPago(pago.id)}
+                    className="bg-red-600 text-white px-4 py-2 rounded-lg"
+                >
+                    Eliminar
+                </button>
+
+            </div>
         </div>
     )
 }
+
+export default PagoCard;
