172e5e6cf77ff15d610409d5150d0e10a14878b4
ArielGnz <arielgon2206@gmail.com>
2026-09-01 20:15:22 -0300

    handleActualizar pago listo

--- client/src/components/pagos/ModalEditarPago.jsx
+++ client/src/components/pagos/ModalEditarPago.jsx
@@ -14,6 +14,29 @@ const ModalEditarPago = ({
     const [fechaPago, setFechaPago] = usetState(pago.fechaPago);
     const [fechaVencimiento, setFechaVencimiento] = useState(pago.fechaVencimiento);
     const [importe, setImporte] = useState(pago.importe);
+    const [estado, setEstado] = useState(pago.estado);
+    const [socioId, setSocioId] = useState(pago.socioId);
+    const [planId, setPlanId] = usestate(pago.planId);
+
+    const opcionesSocios = socios.map((socio) => ({
+        id: socio.id,
+        nombre: `${socio.nombre} ${socio.apellido}`
+    }));
+
+    const handleActualizar = async () => {
+        await actualizarPago(pago.id, {
+            fechaPago,
+            fechaVencimiento,
+            importe,
+            estado,
+            socioId,
+            planId,
+        });
+
+        setPagoEditando(null);
+    }
+
     
+
 
 }
